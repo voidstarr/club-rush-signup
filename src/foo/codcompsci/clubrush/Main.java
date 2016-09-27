5d6ef287e4f7a57d36f09a41926f8451a40ba126
voidstar <voidstarr@users.noreply.github.com>
2016-09-27 15:21:57 -0700

    yeah..

--- src/foo/codcompsci/clubrush/Main.java
+++ src/foo/codcompsci/clubrush/Main.java
@@ -1,10 +1,33 @@
 package foo.codcompsci.clubrush;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static String homeDir = System.getProperty("user.home");
+	static String pathToList = homeDir + File.pathSeparator + "cod-comp-sci-new-members.txt";
+	static File newMembers = new File(pathToList);
 
+	public static void main(String[] args) {
+
+		Window window = new Window();
+		window.frame.setVisible(true);
+	}
+
+	public static void memberEntered(String name, String email) {
+		try (FileWriter fw = new FileWriter(newMembers, true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw)) {
+			out.println(name + " : " + email);
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
