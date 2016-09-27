1a1d669dc6142e76087eb7ebdb98f5f6c2923061
voidstarr <voidstarr@users.noreply.github.com>
2016-09-27 16:29:56 -0700

    this works pretty well

--- src/foo/codcompsci/clubrush/Main.java
+++ src/foo/codcompsci/clubrush/Main.java
@@ -1,3 +1,4 @@
+
 package foo.codcompsci.clubrush;
 
 import java.io.BufferedWriter;
@@ -9,11 +10,10 @@ import java.io.PrintWriter;
 public class Main {
 
 	static String homeDir = System.getProperty("user.home");
-	static String pathToList = homeDir + File.pathSeparator + "cod-comp-sci-new-members.txt";
+	static String pathToList = homeDir + File.separator + "cod-comp-sci-new-members.txt";
 	static File newMembers = new File(pathToList);
 
 	public static void main(String[] args) {
-
 		Window window = new Window();
 		window.frame.setVisible(true);
 	}
--- src/foo/codcompsci/clubrush/Window.java
+++ src/foo/codcompsci/clubrush/Window.java
@@ -4,23 +4,27 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import net.miginfocom.swing.MigLayout;
 
-public class Window implements KeyListener, ActionListener {
+public class Window implements ActionListener {
 
 	public JFrame frame;
 	private JTextField textField;
 	private JTextField textField2;
 	private JButton btnSubmit;
+	JLabel lblThanks;
+
+	Timer visibleTimer = new Timer();
 
 	/**
 	 * Create the application.
@@ -45,11 +49,13 @@ public class Window implements KeyListener, ActionListener {
 		JLabel lblMessage = new JLabel(
 				"<html><center>This is a program thrown together quickly to get your name and email address!<br>If you're interested in joining the club please add yourself to the list to receive our updates.</center></html>");
 		lblMessage.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblMessage.setVerticalAlignment(SwingConstants.CENTER);
+		lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(lblMessage, BorderLayout.NORTH);
 
 		JPanel innerPanel = new JPanel();
 		panel.add(innerPanel);
-		innerPanel.setLayout(new MigLayout("", "[grow,center]", "[][][center][][][]"));
+		innerPanel.setLayout(new MigLayout("", "[grow,center]", "[][][center][][][][]"));
 
 		JLabel lblName = new JLabel("Name:");
 		innerPanel.add(lblName, "flowx,cell 0 2");
@@ -57,7 +63,6 @@ public class Window implements KeyListener, ActionListener {
 		textField = new JTextField();
 		innerPanel.add(textField, "cell 0 2,growx,aligny center");
 		textField.setColumns(40);
-		textField.addActionListener(this);
 
 		JLabel lblEmail = new JLabel("Email:");
 		innerPanel.add(lblEmail, "flowx,cell 0 3");
@@ -65,36 +70,41 @@ public class Window implements KeyListener, ActionListener {
 		textField2 = new JTextField();
 		innerPanel.add(textField2, "cell 0 3,alignx center,aligny center");
 		textField2.setColumns(40);
-		textField2.addActionListener(this);
 
 		btnSubmit = new JButton("Submit");
 		innerPanel.add(btnSubmit, "cell 0 5");
+
+		lblThanks = new JLabel("Thanks, !");
+		lblThanks.setVisible(false);
+		innerPanel.add(lblThanks, "cell 0 6");
 		btnSubmit.addActionListener(this);
 	}
 
 	private void submit() {
-		Main.memberEntered(textField.getText(), textField.getText());
+
+		String name = textField.getText();
+		String email = textField.getText();
+		if (name.equalsIgnoreCase("") || email.equalsIgnoreCase("")) {
+			return;
+		}
+		Main.memberEntered(name, email);
+		lblThanks.setText("Thanks, " + name + "!");
+		lblThanks.setVisible(true);
+		textField.setText("");
+		textField2.setText("");
+		visibleTimer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				lblThanks.setVisible(false);
+
+			}
+		}, 5000);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnSubmit) {
-			System.out.println(e.getActionCommand());
-		}
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+		if (e.getSource() == btnSubmit && e.getActionCommand().toLowerCase().equals("submit")) {
 			submit();
 		}
 
